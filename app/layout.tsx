da7a409f4751679afd058465a222e171b989a7d5
tausiq2003 <tausiqsamantaray03@gmail.com>
2024-11-12 23:14:11 +0530

    meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,15 @@ import { MenuBar } from "@/app/ui/components/Footer";
 export const metadata: Metadata = {
     title: "Tausiq Samantaray",
     description: "portfolio of tausiq samantaray",
+    publisher: "Tausiq Samantaray",
+    keywords: [
+        "Tausiq Samantaray",
+        "portfolio",
+        "about",
+        "projects",
+        "blogs",
+        "contact",
+    ],
 };
 
 export default function RootLayout({
