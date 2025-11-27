f9a3903e3023afd41552eadbd4b2adaefaa588dd
Tausiq Samantaray <tausiqsamantaray03@gmail.com>
2025-11-27 14:35:59 +0530

    Revise project details and add new entries
    Updated project details and descriptions in projdetails.ts, added new projects, and modified existing ones.

--- app/projects/projdetails.ts
+++ app/projects/projdetails.ts
@@ -1,4 +1,49 @@
 export const projDetails = [
+      {
+        name: "project-camp-backend",
+        link: "https://github.com/tausiq2003/moments",
+        description: "To be added",
+        deployedLink: "",
+        youtubeDemo: "",
+        article: "",
+        from: "Nov 2025",
+        to: "Present",
+        skills: ["typescript", "nextjs", "postgres"],
+      },
+      {
+        name: "project-camp-backend",
+        link: "https://github.com/tausiq2003/bp-2-project-mgmt",
+        description: "https://github.com/tausiq2003/bp-2-project-mgmt/blob/master/PRD.md",
+        deployedLink: "",
+        youtubeDemo: "",
+        article: "",
+        from: "Sept 2025",
+        to: "Oct 2025",
+        skills: ["typescript", "expressjs", "mongodb", "nodejs", "docker"],
+     },
+     {
+        name: "videotube",
+        link: "https://github.com/tausiq2003/bp-3-youtube-backend",
+        description: "This is a backend focused project build with Typescript, Express, MongoDB, and with Zod validation. Images and videos are first stored on server with Multer then deleted after uploading to cloudinary. Authentication is JWT-based, which consists of access tokens and refresh tokens, where access tokens are used to carry the authorized requests and refresh tokens are used to refresh access tokens. A user can create, update its profile. Also, user can upload videos, create tweets, create comments for videos and tweets, can like videos, tweets and comments and dislike too. A user can create playlists where it can keep videos. This project has 7 MongoDB documents: users, videos, tweets, playlists,comments, likes, subscriptions. users has all the user information and watch history too. videos has all the videos uploaded by different users and there duration and views. tweets has all the tweets uploaded by different users. playlists are all the playlists uploaded by the users and they are public. comments contains the all-time comments posted by different users on videos and tweets. likes contains all the documents of videos, tweets and comments. subscriptions stores all the subscriber, subscription data in document.
+",
+        deployedLink: "",
+        youtubeDemo: "",
+        article: "",
+        from: "Sept 2025",
+        to: "Sept 2025",
+        skills: ["typescript", "expressjs", "mongodb", "nodejs", "docker"],
+     },
+     {
+        name: "url-shortner-backend",
+        link: "https://github.com/tausiq2003/bp-1-url-shortener",
+        description: "This is a backend focused project made with Typescript, Express, Postgres, with Zod validation using Drizzle ORM. This project is a url-shortener project which takes short code and target url and assigns the short code and redirects to the target url when we GET to the short code. This project has two tables, users and urls, where users stores the user details, and urls stores the url details (about short code and target url). Authentication is done through jwt.",
+        deployedLink: "",
+        youtubeDemo: "",
+        article: "",
+        from: "Sept 2025",
+        to: "Sept 2025",
+        skills: ["typescript", "expressjs", "postgresql", "nodejs", "docker"],
+    }, 
     {
         name: "Chess-Analyze",
         link: "https://github.com/tausiq2003/analyze-chess",
@@ -10,52 +55,31 @@ export const projDetails = [
         from: "Mar 2025",
         to: "Jul 2025",
         skills: ["nextjs", "webworkers", "stockfish", "typescript"],
-    },
-    {
-        name: "url-shortner-backend",
-        link: "https://github.com/tausiq2003/bp-1-url-shortener",
-        description: "This is a backend focused project made with Typescript, Express, Postgres, with Zod validation using Drizzle ORM. This project is a url-shortener project which takes short code and target url and assigns the short code and redirects to the target url when we GET to the short code. This project has two tables, users and urls, where users stores the user details, and urls stores the url details (about short code and target url). Authentication is done through jwt.",
-        deployedLink: "",
-        youtubeDemo: "",
-        article: "",
-        from: "Sept 2025",
-        to: "Sept 2025",
-        skills: ["typescript", "expressjs", "postgresql", "nodejs"],
-    },
+    }
+   
 ];
 const sideProjdetails = [
     {
-        name: "Newsletter System",
-        link: "https://github.com/tausiq2003/cs50-final-project",
-        deployedLink: "",
-        youtubeDemo: "https://www.youtube.com/watch?v=tTEnW0iJ8jg",
-        description:
-            " One Liner: This is my final project for cs50x, and I have implementated a newsletter system. Don't worry, it will send you email once.",
-        from: "Dec 2022",
-        to: "Dec 2022",
-        skills: ["python", "html", "css"],
-    },
-    {
-        name: "Blogsconn",
-        link: "https://github.com/tausiq2003/blogsconn",
+        name: "shell",
+        link: "https://github.com/tausiq2003/shell",
         deployedLink: "",
         youtubeDemo: "",
         description:
-            "A blogging website made in PHP and Frontend, with sql which allows users to create blogs and has admin panel to control it",
-        from: "Apr 2023",
-        to: "May 2023",
-        skills: ["php", "css", "html", "sql"],
+            "To be added",
+        from: "Nov 2025",
+        to: "Present",
+        skills: ["golang", "linux"],
     },
     {
-        name: "discord translation bot",
-        link: "https://github.com/tausiq2003/discord-translator-bot",
+        name: "Twitter Extension",
+        link: "",
         deployedLink: "",
-        youtubeDemo: "https://www.youtube.com/watch?v=2aNhx7srtIQ",
+        youtubeDemo: "https://www.youtube.com/watch?v=vuIWW5NB03o",
         description:
-            "A discord bot that is able to translate hindi to english, with the help of Azure AI Translation API",
-        from: "Aug 2023",
-        to: "Aug 2023",
-        skills: ["python"],
+            "This is a twitter extension that removes distraction from your home feed, the unwanted trending sidebar, and the videos, which is completely useless in a web version in explore page. Remember this twitter extension only works on chrome browser only. I haven't release yet on chrome web store so this is somewhat painful if you ain't a developer. TLDR to the youtube video.",
+        from: "Feb 2024",
+        to: "Feb 2024",
+        skills: ["html", "css", "javascript"],
     },
     {
         name: "Tic-Tac-Toe",
@@ -69,16 +93,37 @@ const sideProjdetails = [
         skills: ["tkinter", "python"],
     },
     {
-        name: "Twitter Extension",
-        link: "",
+        name: "discord translation bot",
+        link: "https://github.com/tausiq2003/discord-translator-bot",
         deployedLink: "",
-        youtubeDemo: "https://www.youtube.com/watch?v=vuIWW5NB03o",
+        youtubeDemo: "https://www.youtube.com/watch?v=2aNhx7srtIQ",
         description:
-            "This is a twitter extension that removes distraction from your home feed, the unwanted trending sidebar, and the videos, which is completely useless in a web version in explore page. Remember this twitter extension only works on chrome browser only. I haven't release yet on chrome web store so this is somewhat painful if you ain't a developer. TLDR to the youtube video.",
-        from: "Feb 2024",
-        to: "Feb 2024",
-        skills: ["html", "css", "javascript"],
+            "A discord bot that is able to translate hindi to english, with the help of Azure AI Translation API",
+        from: "Aug 2023",
+        to: "Aug 2023",
+        skills: ["python"],
     },
-    //fuck those projects
+    {
+        name: "Blogsconn",
+        link: "https://github.com/tausiq2003/blogsconn",
+        deployedLink: "",
+        youtubeDemo: "",
+        description:
+            "A blogging website made in PHP and Frontend, with sql which allows users to create blogs and has admin panel to control it",
+        from: "Apr 2023",
+        to: "May 2023",
+        skills: ["php", "css", "html", "sql"],
+    },
+    {
+        name: "Newsletter System",
+        link: "https://github.com/tausiq2003/cs50-final-project",
+        deployedLink: "",
+        youtubeDemo: "https://www.youtube.com/watch?v=tTEnW0iJ8jg",
+        description:
+            " One Liner: This is my final project for cs50x, and I have implementated a newsletter system. Don't worry, it will send you email once.",
+        from: "Dec 2022",
+        to: "Dec 2022",
+        skills: ["python", "html", "css"],
+    }
 ];
 export const sideprojdetails = sideProjdetails.reverse();
